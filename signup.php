09f19ed8c0ed97126ab522de2e274c9cb5bbe382
jaden <amagicpotato@hotmail.com>
2017-03-31 08:57:41 -0700

    backround fixed

--- signup.php
+++ signup.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HOOP</title>
+    <link rel="stylesheet" type="text/css" href="CSS\HoopTitle.css">
+
 </head>
 <body>
     <p>Registry</p>
     <form>
+        <input type="text" name="Username" placeholder="Username">
   
     </form>
     
