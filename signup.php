228fb149884b6b1d428509151ccbe58e86ae1222
CoolNiggus <ctrlaltelite1.0@hotmail.com>
2017-03-31 11:13:49 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sign up</title>
+    <title>HOOP</title>
 </head>
 <body>
+    <p>Registry</p>
+    <form>
+  
+    </form>
+    
+
 
 </body>
 </html>
