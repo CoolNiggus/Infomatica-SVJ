35adc471606b9887dc1ba79e3e918e9e4ce5da2c
CoolNiggus <AmagicPotato@hotmail.com>
2017-04-01 06:57:52 -0700

    added signup stuff

---
+++ signup_uploader.php
@@ -0,0 +1,17 @@
+<?php
+
+include "dbh.php";
+
+$username = $_POST["username"];
+$email = $_POST["email"];
+$pwd = $_POST["pwd"];
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+
+$sql = "INSERT INTO user(username, email, pwd, firstname, lastname) 
+VALUES('$username','$email','$pwd','$firstname','$lastname')";
+
+$result = mysqli_query($conn, $sql);
+
+header("Location: login.html");
+
