b40f9e5fe23cd032e4c02744c22a6b2963f33f5e
CoolNiggus <AmagicPotato@hotmail.com>
2017-04-01 09:33:24 -0700

    #

--- login_uploader.php
+++ login_uploader.php
@@ -10,6 +10,10 @@ $sql = "SELECT * FROM user WHERE username = '$username' AND pwd = '$pwd' ";
 $result = mysqli_query($conn, $sql);
 
 if($row = mysqli_fetch_assoc($result)){
+
+    session_start;
+    $_SESSION['username'] = $username;
+
     header("Location: homepage.html");
 } else {
     echo "wrong username or password!";
