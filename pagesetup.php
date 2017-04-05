3cdb79a8f3e944ee6a20c7b86adcee385145df86
CoolNiggus <AmagicPotato@hotmail.com>
2017-04-05 16:43:38 -0700

    #

---
+++ pagesetup.php
@@ -0,0 +1,12 @@
+<?php
+
+//use this (inculde) at the very beginning of every pagefile that the user need to be logged
+//on to to use.
+
+session_start();
+
+if(!(isset($_SESSION['CurrentUser']))){
+    header("Location: login.php")
+}
+
+?>
