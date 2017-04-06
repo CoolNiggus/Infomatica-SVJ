cb465179f4f9ff7a33dd5927ecc6608f61a87470
CoolNiggus <AmagicPotato@hotmail.com>
2017-04-05 17:42:33 -0700

    #

--- signup.php
+++ signup.php
@@ -12,18 +12,20 @@
 </head>
 <body class="login">
 
-    <div id="HoopTitle">Hoop</div>
+    
    
-    <form class="login" action="signup_uploader.php" method="POST">
+    <form class="login" action="" method="POST">
         <input class="login" type="text" name="username" placeholder="Username"> <!--Checking if the username is not taken-->
+        <input class="login" type="text" name="email" placeholder="E-Mail"> <!--Checking if E-Mail Address is not used-->
         <input class="login" type="text" name="firstname" placeholder="First Name">
         <input class="login" type="text" name="lastname" placeholder="Last Name">
-        <input class="login" type="text" name="email" placeholder="E-Mail"> <!--Checking if E-Mail Address is not used-->
         <input class="login" type="password" name="pwd" placeholder="Password"> <!--Double checking of password??-->
         <input class="login" type="text" placeholder="School"> <!--Add a list of options-->
         <input class="login" type="date" placeholder="When were you born dude?"> <!--perhaps 3 inline boxes; |dd|mm|yy| ???-->       
         <button id="login" type="submit">Submit</button>
     </form>  
+
+    <p><?php include "signup_uploader.php" ?></p>
    
 </body>
 </html>
--- signup_uploader.php
+++ signup_uploader.php
@@ -11,17 +11,21 @@ $lastname = $_POST["lastname"];
 //checks if the form is valid
 
 $checkusername = (isset($_POST['username']) && !(empty($_POST['username'])) );
+$checkemail = (isset($_POST['email']) && !(empty($_POST['email'])) );
+$checkpwd = (isset($_POST['pwd']) && !(empty($_POST['pwd'])) );
+$checkfirstname = (isset($_POST['firstname']) && !(empty($_POST['firstname'])) );
+$checklastname = (isset($_POST['lastname']) && !(empty($_POST['lastname'])) );
 
-if($checkusername){
-    echo 'yay';
-} else{
-    echo 'nay';
+if($checkusername && $checkemail && $checkpwd && $checkfirstname && $checklastname){
+    $sql = "INSERT INTO user(username, email, pwd, firstname, lastname) 
+    VALUES('$username','$email','$pwd','$firstname','$lastname')";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo "congratulations, your account has been made!"; //note: doesnt check if username already exists
+
+} else {
+    echo "some requiered fields were left open";
 }
-
-//$sql = "INSERT INTO user(username, email, pwd, firstname, lastname) 
-//VALUES('$username','$email','$pwd','$firstname','$lastname')";
-
-//$result = mysqli_query($conn, $sql);
-
 
 ?>
