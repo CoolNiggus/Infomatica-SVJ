c7d3c094c6474834713d56da67a6a14a0aa1d1d1
Stephany Giel <Stephany Giel>
2017-04-12 07:14:07 -0700

    #

--- DatabaseMaker.php
+++ DatabaseMaker.php
@@ -1,6 +1,6 @@
 <?php
 
-echo 'creating database...';
+echo 'creating database... <br>';
 
 $host = "localhost"; //need to be an admin on the database service
 $dbusername = "root";
@@ -12,9 +12,13 @@ $link = mysqli_connect($host, $dbusername, $dbpassword);
 //removes any old version of the database and makes a new one
 $sql =  'DROP DATABASE hoop';
 $result = mysqli_query($link, $sql);
+echo "removed any previuos instance of hoop data base. <br>";
+
 
 $sql = 'CREATE DATABASE hoop';
 $result = mysqli_query($link, $sql);
+echo "created new database hoop. <br>";
+
 
 //updates connect to the new database
 $link = mysqli_connect($host, $dbusername, $dbpassword, 'hoop');
