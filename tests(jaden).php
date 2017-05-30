6ef387a55c7fbb3846aa9bb4796c965b50e8136b
CoolNiggus <AmagicPotato@hotmail.com>
2017-05-29 20:04:16 -0700

    !

--- tests(jaden).php
+++ tests(jaden).php
@@ -1,5 +1,12 @@
 <?php
 session_start();
 
-$sql = 
+include("dbh.php");
+
+$sql = "SELECT * FROM `usertests` WHERE `userID` LIKE 'AmagicPotato'";
+$result = mysqli_query($conn, $sql);
+
+$row = mysqli_fetch_assoc($result);
+print_r($row);
+
 
