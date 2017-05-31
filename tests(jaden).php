5ea08d4dd0c82a753ba11906b795301b526fa1de
CoolNiggus <AmagicPotato@hotmail.com>
2017-05-30 18:54:14 -0700

    1

--- tests(jaden).php
+++ tests(jaden).php
@@ -15,12 +15,32 @@ while($row = mysqli_fetch_assoc($result)){  //gets all of the users testID's
     $testIDs[] = $row['testID'];
 }
 
-foreach ($testIDs as $value) {
+foreach ($testIDs as $value) { //displays all the tests
     $sql = "SELECT * FROM `tests` WHERE `ID` = {$value}";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
-    echo $row['testname'];
-    echo("<br>");
+    //echo($row['testname'] . "<br>");
+
+    //creates a table with the info
+    echo
+     '<table style="width:50%">
+        <tr>
+            <th>testname</th>
+            <th>time</th>
+            <th>creator</th>
+        </tr>
+        <tr>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+        </tr>
+        <tr>
+            <td>Eve</td>
+            <td>Jackson</td>
+            <td>94</td>
+        </tr>
+        </table>'; 
+
 }
 
 
