8ae42c25c7cd519dd6afe37b2186b183b2fe6937
CoolNiggus <AmagicPotato@hotmail.com>
2017-06-05 19:19:07 -0700

    1

--- createanswers.php
+++ createanswers.php
@@ -8,10 +8,11 @@ $username = $_SESSION["CurrentUser"];
 $testname = $_SESSION['testname'];
 $numquestions = $_SESSION['numquestions'];
 
+//array with all the question texts where the question number equals the index of the array
 $questiontexts = array();
 $numanswers = array();
 
-//fills in the above arrays with the question texts and amountof answers per question
+//fills in the above arrays with the question texts and amount of answers per question
 for ($i=1; $i <= $numquestions; $i++) {
     $fieldname1 = "questiontext_".$i; 
     $questiontexts[$i] = $_POST[$fieldname1];
@@ -19,8 +20,17 @@ for ($i=1; $i <= $numquestions; $i++) {
     $numanswers[$i] = $_POST[$fieldname2];
 }
 
+//displays the questions and area to fill in the answers
+echo('<form action="" method="POST">');
+for ($i=1; $i <= $numquestions; $i++) { 
+    echo($questiontexts[$i]."<br>");
+    for ($j=1; $j <= $numanswers[$i]; $j++) { 
+        //creates an input text field with input name = q<questionnumber>a<answernumber>
+        echo('<input type="text" name="q'.$i.'a'.$j.'" placeholder="Answer Text"><br>');
+    }
+    echo("<br>");
+}
+echo('<input type="submit" name="submit" value="Next"> </form>');
 
-print_r($questiontexts);
-print_r($numanswers);
 
 
