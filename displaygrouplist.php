c8b603958c9ed8e6565a57a4b75f7137632de175
Stephany Giel <Stephany Giel>
2017-06-07 10:27:08 -0700

    jj

--- displaygrouplist.php
+++ displaygrouplist.php
@@ -12,9 +12,10 @@ $groupIDs = array();
 
 while($row = mysqli_fetch_assoc($result)){  
     $groupIDs[] = $row['groupID'];
+    }
 
 echo ("<table style='width:50%'>
-      <tr> <th>Group Name</th><th>Creator</th> </tr>");
+      <tr> <th>Group Name</th> </tr>");
 
 foreach ($groupIDs as $value) { 
     $sql = "SELECT * FROM `groups` WHERE `ID` ={$value}";
@@ -22,5 +23,10 @@ foreach ($groupIDs as $value) {
     $row = mysqli_fetch_assoc($result);
 
 $cell1 = $row['name'];
-    $cell1 = "<a href='taketest.php?test={$value}'>{$cell1}</a>"; //creates a link to take the test
-    $cell2 = $row['creatorID'];
+
+
+echo("<tr> <td>{$cell1}</td> </tr>"); 
+
+}
+
+echo ("</table> <br>");
