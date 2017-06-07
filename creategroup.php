f91b0663a0d3279859d0847d49a209b845019c61
CoolNiggus <AmagicPotato@hotmail.com>
2017-06-07 14:20:30 -0700

    1

--- creategroup.php
+++ creategroup.php
@@ -6,18 +6,15 @@ include("dbh.php");
 $groupname = $_POST["name"];
 $username = $_SESSION["CurrentUser"];
 
-
 //Creates group and adds it to database
-$query = "INSERT INTO `hoop`.`groups` (`ID`, `name`) VALUES (NULL, '$groupname')";
+$query = "INSERT INTO `hoop`.`groups` (`ID`, `name`, `adminID`) VALUES (NULL, '$groupname', '$username')";
 $result = mysqli_query($conn, $query);
 $groupID = mysqli_insert_id($conn);
-$_SESSION["groupID"] = $groupID;
 
 //adds currentuser to the group
 $query="INSERT INTO `hoop`.`groupusers` (`groupID`, `userID`) VALUES ('$groupID', '$username')";
 $result = mysqli_query($conn, $query);
 header("Location: grouplist.php");
-
 
 
 
